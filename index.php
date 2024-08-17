d43da12f8fc2d587c3bec19db216704f6020ff76
Jeremias <ianigroj@gmail.com>
2024-08-17 17:16:44 -0300

    hicimos varios avances

--- index.php
+++ index.php
@@ -4,81 +4,172 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" 
-    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
-    <link rel="stylesheet" href="styles.css">
+    integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Bebas+Neue&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="css/styles.css">
     <title>Películas IRUPE</title>
 </head>
 <body>
-     
     <header>
-        <nav class="navbar navbar-dark bg-dark">
-            <div class="container-fluid">
-                <a class="navbar-brand" href="#">LOGO IRUPE</a>
-                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                    <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                        <li class="nav-item">
-                            <a class="nav-link active" aria-current="page" href="#">IRUPE</a>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link" href="#">Quienes Somos</a>
-                        </li>
-                        <li class="nav-item dropdown">
-                            <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                Producciones
-                            </a>
-                            <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                <li><a class="dropdown-item" href="#">Películas</a></li>
-                                <li><a class="dropdown-item" href="#">Series</a></li>
-                            </ul>
-                        </li>
-                        <li class="nav-item">
-                            <a class="nav-link disabled">Disabled</a>
-                        </li>
-                    </ul>
-                    <form class="d-flex">
-                        <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-                        <button class="btn btn-outline-success" type="submit">Buscar</button>
-                    </form>
+        <section class="BarraNavegacion">
+            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+                <div class="container-fluid">
+                    <a class="navbar-brand" href="#">LOGO IRUPE</a>
+                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                            <li class="nav-item">
+                                <a class="nav-link active" aria-current="page" href="#">IRUPE</a>
+                            </li>
+                            <li class="nav-item">
+                                <a class="nav-link" href="#">Quienes Somos</a>
+                            </li>
+                            <li class="nav-item dropdown">
+                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                                    Producciones
+                                </a>
+                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                                    <li><a class="dropdown-item" href="#">Películas</a></li>
+                                    <li><a class="dropdown-item" href="#">Series</a></li>
+                                </ul>
+                            </li>
+                            <!-- <li class="nav-item">
+                                <a class="nav-link disabled">Disabled</a>
+                            </li> -->
+                        </ul>
+                        <!-- <form class="d-flex">
+                            <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
+                            <button class="btn btn-outline-success" type="submit">Buscar</button>
+                        </form> -->
+                    </div>
                 </div>
-            </div>
-        </nav>
-
-        <div class="video-container mt-4">
-            <iframe width="560" height="315" src="https://www.youtube.com/embed/T_QAxvPjjBc" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-        </div>
+            </nav>
+        </section>
     </header>
-
     <main>
-        <div class="container-fluid text-center mt-5">
-            <h2>Películas</h2>
-        </div>
+        <section class="principal">
+            <div class="video-container">
+            <iframe src="https://www.youtube.com/embed/FHh_2EN7zh0?autoplay=1&mute=1&loop=1&playlist=FHh_2EN7zh0&controls=0&showinfo=0&modestbranding=1&iv_load_policy=3&rel=0&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+            </div>
 
-        <div class="container-fluid mt-4">
-            <div class="row">
-                <div class="col-12 col-md-6 mb-4">
-                    <a href="pelicula1.html">
-                        <div class="card">
-                            <img src="img/1.jpg" class="card-img-top" alt="Pelicula 1">
-                        </div>
-                    </a>
-                </div>
-                <div class="col-12 col-md-6 mb-4">
-                    <a href="pelicula2.html">
-                        <div class="card">
-                            <img src="img/2.jpg" class="card-img-top" alt="Pelicula 2">
+            <h1>LOGO IRUPE</h1><br>
+          
+        </section>
+
+        <section class="direccion">
+
+            <a href="pagina-a.html"><h2>AUDIVISUAL</h2></a>
+            <a href="pagina-b.html"><h2>TEATRO</h2></a>
+            <a href="pagina-c.html"><h2>EQUIPO</h2></a>
+            <a href="pagina-d.html"><h2>CONTACTO</h2></a>
+
+        </section>
+
+        <!-- seccion cards -->
+         
+        <section class="CardsPeliculas py-5">
+    <div class="container">
+        <div class="row">
+            <!-- Card 1 -->
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <a href="pagina-pelicula-1.html" class="card-link">
+                        <img src="img/1.jpg" class="card-img-top" alt="Película 1">
+                        <div class="card-info">
+                            <h5 class="card-title">Título de la Película 1</h5>
+                            <p class="card-text">Descripción breve de la película.</p>
+                            <a href="pagina-pelicula-1.html" class="btn btn-primary">Más información</a>
                         </div>
                     </a>
                 </div>
             </div>
-            <!-- Puedes agregar más filas con más películas aquí -->
+            <!-- Card 2 -->
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <a href="pagina-pelicula-2.html" class="card-link">
+                        <img src="img/1.jpg" class="card-img-top" alt="Película 2">
+                        <div class="card-info">
+                            <h5 class="card-title">Título de la Película 2</h5>
+                            <p class="card-text">Descripción breve de la película.</p>
+                            <a href="pagina-pelicula-2.html" class="btn btn-primary">Más información</a>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <!-- Card 3 -->
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <a href="pagina-pelicula-3.html" class="card-link">
+                        <img src="img/1.jpg" class="card-img-top" alt="Película 3">
+                        <div class="card-info">
+                            <h5 class="card-title">Título de la Película 3</h5>
+                            <p class="card-text">Descripción breve de la película.</p>
+                            <a href="pagina-pelicula-3.html" class="btn btn-primary">Más información</a>
+                        </div>
+                    </a>
+                </div>
+            </div>
         </div>
+    </div>
+</section>
+
+<section class="cardsSeries py-5 bg-light">
+    <div class="container">
+        <h3 class="text-center mb-4">Series</h3>
+        <div class="row">
+            <!-- Card 1 -->
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <a href="pagina-serie-1.html" class="card-link">
+                        <img src="img/1.jpg" class="card-img-top" alt="Serie 1">
+                        <div class="card-info">
+                            <h5 class="card-title">Título de la Serie 1</h5>
+                            <p class="card-text">Descripción breve de la serie.</p>
+                            <a href="pagina-serie-1.html" class="btn btn-primary">Más información</a>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <!-- Card 2 -->
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <a href="pagina-serie-2.html" class="card-link">
+                        <img src="img/1.jpg" class="card-img-top" alt="Serie 2">
+                        <div class="card-info">
+                            <h5 class="card-title">Título de la Serie 2</h5>
+                            <p class="card-text">Descripción breve de la serie.</p>
+                            <a href="pagina-serie-2.html" class="btn btn-primary">Más información</a>
+                        </div>
+                    </a>
+                </div>
+            </div>
+            <!-- Card 3 -->
+            <div class="col-md-4 mb-4">
+                <div class="card">
+                    <a href="pagina-serie-3.html" class="card-link">
+                        <img src="img/1.jpg" class="card-img-top" alt="Serie 3">
+                        <div class="card-info">
+                            <h5 class="card-title">Título de la Serie 3</h5>
+                            <p class="card-text">Descripción breve de la serie.</p>
+                            <a href="pagina-serie-3.html" class="btn btn-primary">Más información</a>
+                        </div>
+                    </a>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+
     </main>
 
-    <footer>
-        <!-- Aquí puedes añadir contenido para el pie de página -->
+    <footer class="bg-dark text-white text-center py-3">
+        <h5>Acá van los enlaces de las redes sociales</h5>
     </footer>
 
     <script src="https://cdn.jsdelivr.net/npm/@popperjs/core@2.10.2/dist/umd/popper.min.js" 
@@ -87,6 +178,3 @@
     integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 </html>
-
-
-
