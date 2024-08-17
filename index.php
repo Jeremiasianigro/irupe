5dc0a93cd767544ffb530cea3eabdb86720f7980
Jeremias <ianigroj@gmail.com>
2024-08-17 18:48:38 -0300

    hicimos el logo

--- index.php
+++ index.php
@@ -13,159 +13,152 @@
 </head>
 <body>
     <header>
-        <section class="BarraNavegacion">
-            <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
-                <div class="container-fluid">
-                    <a class="navbar-brand" href="#">LOGO IRUPE</a>
-                    <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
-                        <span class="navbar-toggler-icon"></span>
-                    </button>
-                    <div class="collapse navbar-collapse" id="navbarSupportedContent">
-                        <ul class="navbar-nav me-auto mb-2 mb-lg-0">
-                            <li class="nav-item">
-                                <a class="nav-link active" aria-current="page" href="#">IRUPE</a>
-                            </li>
-                            <li class="nav-item">
-                                <a class="nav-link" href="#">Quienes Somos</a>
-                            </li>
-                            <li class="nav-item dropdown">
-                                <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
-                                    Producciones
-                                </a>
-                                <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
-                                    <li><a class="dropdown-item" href="#">Películas</a></li>
-                                    <li><a class="dropdown-item" href="#">Series</a></li>
-                                </ul>
-                            </li>
-                            <!-- <li class="nav-item">
-                                <a class="nav-link disabled">Disabled</a>
-                            </li> -->
+    <section class="BarraNavegacion">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+        <div class="container-fluid">
+            <!-- Reemplaza el texto por una imagen -->
+            <a class="navbar-brand" href="#">
+                <img src="img/LogoTransparente.png" alt="Logo IRUPE" class="navbar-logo">
+            </a>
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+            <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                    <li class="nav-item">
+                        <a class="nav-link active" aria-current="page" href="#">IRUPE</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Quienes Somos</a>
+                    </li>
+                    <li class="nav-item dropdown">
+                        <a class="nav-link dropdown-toggle" href="#" id="navbarDropdown" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                            Producciones
+                        </a>
+                        <ul class="dropdown-menu" aria-labelledby="navbarDropdown">
+                            <li><a class="dropdown-item" href="#">Películas</a></li>
+                            <li><a class="dropdown-item" href="#">Series</a></li>
                         </ul>
-                        <!-- <form class="d-flex">
-                            <input class="form-control me-2" type="search" placeholder="Buscar" aria-label="Search">
-                            <button class="btn btn-outline-success" type="submit">Buscar</button>
-                        </form> -->
-                    </div>
-                </div>
-            </nav>
-        </section>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</section>
+
     </header>
     <main>
-        <section class="principal">
-            <div class="video-container">
-            <iframe src="https://www.youtube.com/embed/FHh_2EN7zh0?autoplay=1&mute=1&loop=1&playlist=FHh_2EN7zh0&controls=0&showinfo=0&modestbranding=1&iv_load_policy=3&rel=0&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
-            </div>
+    <section class="principal">
+    <div class="video-container">
+        <iframe src="https://www.youtube.com/embed/FHh_2EN7zh0?autoplay=1&mute=1&loop=1&playlist=FHh_2EN7zh0&controls=0&showinfo=0&modestbranding=1&iv_load_policy=3&rel=0&fs=0" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen></iframe>
+    </div>
+    <!-- Reemplaza el texto por una imagen -->
+    <div class="logo-container">
+        <img src="img/LogoTransparente.png" alt="Logo IRUPE" class="logo-image">
+    </div>
+</section>
 
-            <h1>LOGO IRUPE</h1><br>
-          
-        </section>
 
         <section class="direccion">
-
             <a href="pagina-a.html"><h2>AUDIVISUAL</h2></a>
             <a href="pagina-b.html"><h2>TEATRO</h2></a>
             <a href="pagina-c.html"><h2>EQUIPO</h2></a>
             <a href="pagina-d.html"><h2>CONTACTO</h2></a>
-
         </section>
 
         <!-- seccion cards -->
-         
         <section class="CardsPeliculas py-5">
-    <div class="container">
-        <div class="row">
-            <!-- Card 1 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <a href="pagina-pelicula-1.html" class="card-link">
-                        <img src="img/1.jpg" class="card-img-top" alt="Película 1">
-                        <div class="card-info">
-                            <h5 class="card-title">Título de la Película 1</h5>
-                            <p class="card-text">Descripción breve de la película.</p>
-                            <a href="pagina-pelicula-1.html" class="btn btn-primary">Más información</a>
+            <div class="container">
+                <div class="row">
+                    <!-- Card 1 -->
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <a href="pagina-pelicula-1.html" class="card-link">
+                                <img src="img/1.jpg" class="card-img-top" alt="Película 1">
+                                <div class="card-info">
+                                    <h5 class="card-title">Título de la Película 1</h5>
+                                    <p class="card-text">Descripción breve de la película.</p>
+                                    <a href="pagina-pelicula-1.html" class="btn btn-primary">Más información</a>
+                                </div>
+                            </a>
                         </div>
-                    </a>
+                    </div>
+                    <!-- Card 2 -->
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <a href="pagina-pelicula-2.html" class="card-link">
+                                <img src="img/1.jpg" class="card-img-top" alt="Película 2">
+                                <div class="card-info">
+                                    <h5 class="card-title">Título de la Película 2</h5>
+                                    <p class="card-text">Descripción breve de la película.</p>
+                                    <a href="pagina-pelicula-2.html" class="btn btn-primary">Más información</a>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- Card 3 -->
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <a href="pagina-pelicula-3.html" class="card-link">
+                                <img src="img/1.jpg" class="card-img-top" alt="Película 3">
+                                <div class="card-info">
+                                    <h5 class="card-title">Título de la Película 3</h5>
+                                    <p class="card-text">Descripción breve de la película.</p>
+                                    <a href="pagina-pelicula-3.html" class="btn btn-primary">Más información</a>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <!-- Card 2 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <a href="pagina-pelicula-2.html" class="card-link">
-                        <img src="img/1.jpg" class="card-img-top" alt="Película 2">
-                        <div class="card-info">
-                            <h5 class="card-title">Título de la Película 2</h5>
-                            <p class="card-text">Descripción breve de la película.</p>
-                            <a href="pagina-pelicula-2.html" class="btn btn-primary">Más información</a>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- Card 3 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <a href="pagina-pelicula-3.html" class="card-link">
-                        <img src="img/1.jpg" class="card-img-top" alt="Película 3">
-                        <div class="card-info">
-                            <h5 class="card-title">Título de la Película 3</h5>
-                            <p class="card-text">Descripción breve de la película.</p>
-                            <a href="pagina-pelicula-3.html" class="btn btn-primary">Más información</a>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
+        </section>
 
-<section class="cardsSeries py-5 bg-light">
-    <div class="container">
-        <h3 class="text-center mb-4">Series</h3>
-        <div class="row">
-            <!-- Card 1 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <a href="pagina-serie-1.html" class="card-link">
-                        <img src="img/1.jpg" class="card-img-top" alt="Serie 1">
-                        <div class="card-info">
-                            <h5 class="card-title">Título de la Serie 1</h5>
-                            <p class="card-text">Descripción breve de la serie.</p>
-                            <a href="pagina-serie-1.html" class="btn btn-primary">Más información</a>
+        <section class="cardsSeries py-5 bg-light">
+            <div class="container">
+                <h3 class="text-center mb-4">Series</h3>
+                <div class="row">
+                    <!-- Card 1 -->
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <a href="pagina-serie-1.html" class="card-link">
+                                <img src="img/1.jpg" class="card-img-top" alt="Serie 1">
+                                <div class="card-info">
+                                    <h5 class="card-title">Título de la Serie 1</h5>
+                                    <p class="card-text">Descripción breve de la serie.</p>
+                                    <a href="pagina-serie-1.html" class="btn btn-primary">Más información</a>
+                                </div>
+                            </a>
                         </div>
-                    </a>
+                    </div>
+                    <!-- Card 2 -->
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <a href="pagina-serie-2.html" class="card-link">
+                                <img src="img/1.jpg" class="card-img-top" alt="Serie 2">
+                                <div class="card-info">
+                                    <h5 class="card-title">Título de la Serie 2</h5>
+                                    <p class="card-text">Descripción breve de la serie.</p>
+                                    <a href="pagina-serie-2.html" class="btn btn-primary">Más información</a>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
+                    <!-- Card 3 -->
+                    <div class="col-md-4 mb-4">
+                        <div class="card">
+                            <a href="pagina-serie-3.html" class="card-link">
+                                <img src="img/1.jpg" class="card-img-top" alt="Serie 3">
+                                <div class="card-info">
+                                    <h5 class="card-title">Título de la Serie 3</h5>
+                                    <p class="card-text">Descripción breve de la serie.</p>
+                                    <a href="pagina-serie-3.html" class="btn btn-primary">Más información</a>
+                                </div>
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-            <!-- Card 2 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <a href="pagina-serie-2.html" class="card-link">
-                        <img src="img/1.jpg" class="card-img-top" alt="Serie 2">
-                        <div class="card-info">
-                            <h5 class="card-title">Título de la Serie 2</h5>
-                            <p class="card-text">Descripción breve de la serie.</p>
-                            <a href="pagina-serie-2.html" class="btn btn-primary">Más información</a>
-                        </div>
-                    </a>
-                </div>
-            </div>
-            <!-- Card 3 -->
-            <div class="col-md-4 mb-4">
-                <div class="card">
-                    <a href="pagina-serie-3.html" class="card-link">
-                        <img src="img/1.jpg" class="card-img-top" alt="Serie 3">
-                        <div class="card-info">
-                            <h5 class="card-title">Título de la Serie 3</h5>
-                            <p class="card-text">Descripción breve de la serie.</p>
-                            <a href="pagina-serie-3.html" class="btn btn-primary">Más información</a>
-                        </div>
-                    </a>
-                </div>
-            </div>
-        </div>
-    </div>
-</section>
-
-
-
+        </section>
     </main>
 
     <footer class="bg-dark text-white text-center py-3">
@@ -178,3 +171,4 @@
     integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
 </body>
 </html>
+
